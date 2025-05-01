519432ba3f278f34b35795d4741147c9a47383bc
Andrew K <andrewjk@users.noreply.github.com>
2025-05-01 14:50:12 +1000

    Chore: update dependencies

--- site.config.ts
+++ site.config.ts
@@ -1,12 +1,10 @@
-import { cloudflare as socialAdapter } from "@redraft/adapter-cloudflare";
+import { cloudflare, siteAdapter } from "@redraft/adapter-cloudflare";
 import { defineSite } from "@redraft/site";
-import { cloudflare as siteAdapter } from "@torpor/adapter-cloudflare";
 import { Site } from "@torpor/build";
 import "dotenv/config";
 
 const site: Site = new Site();
-site.adapter = siteAdapter;
 
-await defineSite(site, socialAdapter);
+await defineSite(site, cloudflare, siteAdapter);
 
 export default site;
